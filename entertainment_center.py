649b6c79dbffb38eb5c0533ffefd90a07dd2a75b
moscowjh <horowitz.jason@gmail.com>
2015-10-07 23:08:39 -0400

    feat: New classes and new elements within the class
    Added two new elements for each class (movie_stars, release_year) and six movies (classes) for the exercise.

--- entertainment_center.py
+++ entertainment_center.py
@@ -13,17 +13,50 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-# print(toy_story.storyline)
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+bull_durham = media.Movie("Bull Durham",
+                        "A witty blend of sports drama, intelligent comedy and sexy romance Bull Durham is an ideal date film. ",
+                        "https://upload.wikimedia.org/wikipedia/en/e/ef/Bull_Durham_film_poster.jpg",
+                        "https://www.youtube.com/watch?v=dnJFndf-Krg",
+                        "Kevin Costner, Susan Sarandon",
+                        "1988")
+
+moneyball = media.Movie("Moneyball",
+                        "Based on a true story, Moneyball is a movie for anybody who has ever dreamed of taking on the system.",
+                        "https://upload.wikimedia.org/wikipedia/en/2/2e/Moneyball_Poster.jpg",
+                        "https://www.youtube.com/watch?v=BwIBZi1RcNI",
+                        "Brad Pitt, Jonah Hill",
+                        "2011")
+
+badnews_bears = media.Movie("Bad News Bears",
+                        "The success of this underdog comedy from director Michael Ritchie almost single-handedly spawned the kids' sports film boom of the 1980s and '90s.",
+                        "https://upload.wikimedia.org/wikipedia/en/f/f9/Bad_news_bears_1976_movie_poster.jpg",
+                        "https://www.youtube.com/watch?v=sF6De-XP7x4",
+                        "Walter Matthau, Tatum O'Neal",
+                        "1976")
+
+bangthe_drumslowly = media.Movie("Bang the Drum Slowly",
+                        "Bang the Drum Slowly is a touching melodrama that explores the inner workings of a baseball club and its players' personalities with remarkable depth.",
+                        "https://upload.wikimedia.org/wikipedia/en/0/0e/Bang_the_Drum_Slowly_poster.jpg",
+                        "https://www.youtube.com/watch?v=Xa3EdS3l1zs",
+                        "Michael Moriarity, Robert DeNiro",
+                        "1973")
+
+aleague_oftheirown = media.Movie("A League of Their Own",
+                        "Sentimental and light, but still thoroughly charming, A League of Their Own is buoyed by solid performances from a wonderful cast.",
+                        "https://upload.wikimedia.org/wikipedia/en/5/5f/League_of_their_own_ver2.jpg",
+                        "https://www.youtube.com/watch?v=WcN392H2jx0",
+                        "Tom Hanks, Geena Davis, Madonna",
+                        "1992")
+
+eightmen_out = media.Movie("Eight Men Out",
+                        "Bang the Drum Slowly is a touching melodrama that explores the inner workings of a baseball club and its players' personalities with remarkable depth.",
+                        "https://upload.wikimedia.org/wikipedia/en/4/41/Eight_Men_Out_DVD_cover.jpg",
+                        "https://www.youtube.com/watch?v=JSHQw85pvek",
+                        "John Cusack, Charlie Sheen",
+                        "1988")
 #print(avatar.storyline)
 #avatar.show_trailer()
+# print(toy_story.storyline)
 
-movies = [toy_story, avatar]
+movies = [bull_durham, moneyball, badnews_bears, bangthe_drumslowly, aleague_oftheirown, eightmen_out]
 fresh_tomatoes.open_movies_page(movies)
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -107,7 +107,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">Jason's Favorite Baseball Movies</a>
           </div>
         </div>
       </div>
